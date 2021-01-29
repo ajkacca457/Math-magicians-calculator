2708d3b451232bb08720af2d0985b2744cad7b08
Avijit Karmaker <ajkacca@gmail.com>
2021-01-29 19:39:26 +0200

    move app.js file to components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Display from './components/Display';
-import ButtonPanel from './components/ButtonPanel';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 class App extends Component {
   constructor(props) {

8f3e42e124e689bf26a4d87b64ec084c8c3590db
Avijit Karmaker <ajkacca@gmail.com>
2021-02-01 13:57:07 +0200

    add handleclick function and updateResult function

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,26 @@ import calculate from '../logic/calculate';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
+
+  handleClick=buttonName => {
+    const newstate = calculate(this.state, buttonName);
+    this.setState(newstate);
+  }
+
+  updateResult=() => {
+    // eslint-disable-next-line no-unused-vars
+    const { total, next, operation } = this.state;
+    if (next) {
+      return next;
+    }
+    return total;
+  };
 
   render() {
     return (

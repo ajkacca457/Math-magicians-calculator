66ef6422bc085f657abe8dff0a724beefe20f2a5
Avijit Karmaker <ajkacca@gmail.com>
2021-02-05 12:16:11 +0200

    refactored calculator with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
@@ -8,54 +8,51 @@ import Navbar from './Navbar';
 import Calculator from './Calculator';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const data = {
+  total: null,
+  next: null,
+  operation: null,
+};
 
-  handleClick=buttonName => {
-    const newstate = calculate(this.state, buttonName);
-    this.setState(newstate);
-  }
+const App = () => {
+  const [state, setState] = useState(data);
 
-  updateResult=() => {
+  const handleClick = buttonName => {
+    const newstate = calculate(state, buttonName);
+    setState(newstate);
+  };
+
+  const updateResult = () => {
     // eslint-disable-next-line no-unused-vars
-    const { total, next, operation } = this.state;
+    const { total, next, operation } = state;
     if (next) {
       return next;
     }
     return total;
   };
 
-  render() {
-    return (
-      <Router>
-        <div className="app">
-          <Navbar />
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-          <Route exac path="/home" component={Home} />
-          <Route
-            exact
-            path="/calculator"
-            render={() => (
-              <Calculator
-                updateResult={this.updateResult}
-                handleClick={this.handleClick}
-              />
-            )}
-          />
-          <Route exac path="/quote" component={Quote} />
-        </div>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route exac path="/home" component={Home} />
+        <Route
+          exact
+          path="/calculator"
+          render={() => (
+            <Calculator
+              updateResult={updateResult}
+              handleClick={handleClick}
+            />
+          )}
+        />
+        <Route exac path="/quote" component={Quote} />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
